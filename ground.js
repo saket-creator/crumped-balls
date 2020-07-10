98f3d2e9cf32d09c066488c9327a8c3d8fc46788
saket-creator <65777399+saket-creator@users.noreply.github.com>
2020-07-10 12:37:09 +0530

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,18 @@
+class Ground {
+    constructor(x,y,width,height) {
+      var options = {
+          isStatic: true
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+      fill("red");
+      var pos =this.body.position;
+      rectMode(CENTER);
+      this.body.shapeColor = ("white");
+      rect(pos.x, pos.y, this.width, this.height);
+    }
+  };
